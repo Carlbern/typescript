6a03631a479ff918576038c5a2319631af189226
Carlbern <carlgbg031@gmail.com>
2025-01-03 15:49:07 +0100

    Working version of localStorage

---
+++ main.js
@@ -0,0 +1,36 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var p = document.querySelector("p");
+var btnAdd = document.getElementById("add");
+var btnSave = document.getElementById("save");
+var btnClear = document.getElementById("clear");
+var input = document.querySelector("input");
+var textList = [];
+var htmlText = "";
+btnAdd.addEventListener("click", function () {
+    textList.push(input.value);
+    printList(textList);
+    save();
+});
+btnClear.addEventListener("click", function () {
+    textList = [];
+    localStorage.clear();
+    printList(textList);
+});
+function printList(textList) {
+    for (var i = 0; i < textList.length; i++) {
+        htmlText += "<div><span>".concat(textList[i], "</span></div>");
+    }
+    p.innerHTML = htmlText;
+    htmlText = "";
+}
+function save() {
+    localStorage.setItem("mytext", JSON.stringify(textList));
+}
+function load() {
+    if (localStorage.getItem("mytext")) {
+        textList = JSON.parse(localStorage.getItem("mytext"));
+        printList(textList);
+    }
+}
+function remove() { }
---
+++ main.ts
@@ -0,0 +1,39 @@
+export {};
+let p = document.querySelector("p");
+let btnAdd = document.getElementById("add");
+let btnSave = document.getElementById("save");
+let btnClear = document.getElementById("clear");
+let input = document.querySelector("input");
+let textList: any = [];
+let htmlText = "";
+
+btnAdd!.addEventListener("click", () => {
+  textList.push(input!.value);
+  printList(textList);
+  save();
+});
+
+btnClear!.addEventListener("click", () => {
+  textList = [];
+  localStorage.clear();
+  printList(textList);
+});
+
+function printList(textList: any) {
+  for (let i = 0; i < textList.length; i++) {
+    htmlText += `<div><span>${textList[i]}</span></div>`;
+  }
+  p!.innerHTML = htmlText;
+  htmlText = "";
+}
+
+function save() {
+  localStorage.setItem("mytext", JSON.stringify(textList));
+}
+function load() {
+  if (localStorage.getItem("mytext")) {
+    textList = JSON.parse(localStorage.getItem("mytext")!);
+    printList(textList);
+  }
+}
+function remove() {}
